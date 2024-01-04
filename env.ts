f60a8495eb9a6eca4018e3888a9c37ae4c92f01d
John Doe <john.doe@gmail.com>
2024-01-04 18:21:01 +0800

    March first coding

---
+++ env.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv'
+import populateEnv from 'populate-env'
+
+
+
+config()
+
+export let env = {
+    DB_NAME:'',
+    DB_USER:'',
+    DB_PASSWORD:'',
+    SESSION_SECRET:'',
+}
+
+populateEnv(env,{ mode:'halt'})
+
