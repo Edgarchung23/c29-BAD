0e868ea042086bc54403c83e6ed33940cfc02ce5
Edgar <EdgarChung@EdgarMacBook-Air.local>
2024-01-07 18:23:19 +0800

    update

--- db.ts
+++ db.ts
@@ -1,7 +1,9 @@
 import Knex from 'knex'
 import { env } from './env'
 
-let config = require('./knexfile')
-let profile = config[env.NODE_ENV]
-
-export let knex = Knex(profile)
+export function createKnex() {
+  let config = require('./knexfile')
+  let profile = config[env.NODE_ENV]
+  let knex = Knex(profile)
+  return knex
+}
--- user/user.service.impl.ts
+++ user/user.service.impl.ts
@@ -1,24 +1,24 @@
-import { Client } from "pg";
-import { UserService } from "./user.service";
-import { HttpError } from "../http.error";
+// import { Client } from "pg";
+// import { UserService } from "./user.service";
+// import { HttpError } from "../http.error";
 
-export class UserServiceImpl implements UserService {
-  constructor(private client: Client) {}
+// export class UserServiceImpl implements UserService {
+//   constructor(private client: Client) {}
 
-  login(input: {
-    username: string;
-    password: string;
-  }): Promise<{ id: number }> {
-    throw new HttpError(501, "Method not implemented.");
-  }
+//   login(input: {
+//     username: string;
+//     password: string;
+//   }): Promise<{ id: number }> {
+//     throw new HttpError(501, "Method not implemented.");
+//   }
 
-  register(input: {
-    username: string;
-    password: string;
-    avatar: string | null;
-    email: string;
-    tel: string;
-  }): Promise<{ id: number }> {
-    throw new HttpError(501, 'Method not implemented.')
-  }
-}
+//   register(input: {
+//     username: string;
+//     password: string;
+//     avatar: string | null;
+//     email: string;
+//     tel: string;
+//   }): Promise<{ id: number }> {
+//     throw new HttpError(501, 'Method not implemented.')
+//   }
+// }
