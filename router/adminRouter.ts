e6a921e81d14611e84a4e13bd24ce087bc105c55
Edgar <EdgarChung@EdgarMacBook-Air.local>
2024-01-09 13:05:55 +0800

    update

--- router/adminRouter.ts
+++ router/adminRouter.ts
@@ -1,11 +1,11 @@
-import { Router } from "express";
-import { isAdmin } from "../middelware";
+// import { Router } from "express";
+// import { isAdmin } from "../middelware";
 
-export const adminRouter = Router();
+// export const adminRouter = Router();
 
-adminRouter.post("/register", isAdmin, async (req, res)=>{
-    console.log(req.body.email);
-    if (req.body.email == undefined || req.body.email == ""){
-        res.status(400).json({ message: "email can not be null"})
-    }
-})
+// adminRouter.post("/register", isAdmin, async (req, res)=>{
+//     console.log(req.body.email);
+//     if (req.body.email == undefined || req.body.email == ""){
+//         res.status(400).json({ message: "email can not be null"})
+//     }
+// })
