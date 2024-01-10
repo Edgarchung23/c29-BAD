f14343955d8a44b8a30f57ed446fa960f42fb073
Edgar <EdgarChung@EdgarMacBook-Air.local>
2024-01-10 15:06:42 +0800

    update

--- server.ts
+++ server.ts
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static("public/html"));
+app.use(express.static("public/images"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
