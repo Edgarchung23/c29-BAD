78e67d46a1d43181e68d8654b6de2f1ce06989bd
Edgar <EdgarChung@EdgarMacBook-Air.local>
2024-01-12 17:42:17 +0800

    update

--- server.ts
+++ server.ts
@@ -45,6 +45,7 @@ declare module "express-session"{
       username?:string;
   }
 }
+//<-----------APP.USE---------------------------------------------->
 
 app.use(express.static("public/html/"));
 app.use(express.static("public"));
@@ -56,8 +57,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(adminRouter);
 app.use(authRouter);
-//<--------------------------------------------------------------->
-app.get('/', async (req, res) => {
+
+//<-----------TextToSpeech------------------------------------------->
+
+app.get('html/reader.html?book=原子習慣', async (req, res) => {
   const text = '聽君一席話如聽一席話'
 //   const text = '逝世後仍繼續席捲全球的傳奇小說家。一九二三年出生於美國維吉尼亞州。美國史上最暢銷的作家之一，擅長以禁斷戀情、血親糾葛等元素創造膾炙人口的小說，征服一代又一代的讀者。雖然中年才邁入寫作生涯，其獨具風格的創作與驚人的銷售成就令同時期作家皆難望其項背，堪稱「歌德羅曼史」、「家族傳奇」類型女王。一九八六年逝世後，由於「V.C.安德魯絲」之名儼然已經成為一個暢銷書品牌，其親族甚至聘請代筆作家，根據她留下的草稿大綱，至今仍繼續發表新作。'
   const request = {
@@ -77,6 +80,7 @@ app.get('/', async (req, res) => {
     res.status(500).send('Error synthesizing speech');
   }
 });
+//<--------------------------------------------------------------->
 
 
 
