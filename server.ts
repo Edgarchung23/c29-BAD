f08739796d01d8f33df6ce2bdffc65388e85cde7
Edgar <EdgarChung@EdgarMacBook-Air.local>
2024-01-12 17:38:28 +0800

    update

--- server.ts
+++ server.ts
@@ -16,10 +16,17 @@ import { authRouter } from "./router/authRouter";
 // import { adminRouter } from "./router/is_adminRouter";
 // import { isAdmin } from "./middelware";
 import expressSession from "express-session";
-
 export const knex = createKnex()
 const app = express();
 import { router } from "./routes/routes"; 
+
+
+const textToSpeech = require('@google-cloud/text-to-speech');
+const fs = require('fs');
+const keyFile = 'c29-bad-grp3.json'; 
+const client = new textToSpeech.TextToSpeechClient({
+  keyFilename: keyFile
+});
 
 
 //<--------------------------March's need------------------------------------>
@@ -42,7 +49,6 @@ declare module "express-session"{
 app.use(express.static("public/html/"));
 app.use(express.static("public"));
 app.use(express.static("books"));
-
 app.use(express.static("public/images"));
 app.use(express.static("public"));
 app.use("/admin", is_admin, express.static("private"));
@@ -51,7 +57,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(adminRouter);
 app.use(authRouter);
 //<--------------------------------------------------------------->
+app.get('/', async (req, res) => {
+  const text = '聽君一席話如聽一席話'
+//   const text = '逝世後仍繼續席捲全球的傳奇小說家。一九二三年出生於美國維吉尼亞州。美國史上最暢銷的作家之一，擅長以禁斷戀情、血親糾葛等元素創造膾炙人口的小說，征服一代又一代的讀者。雖然中年才邁入寫作生涯，其獨具風格的創作與驚人的銷售成就令同時期作家皆難望其項背，堪稱「歌德羅曼史」、「家族傳奇」類型女王。一九八六年逝世後，由於「V.C.安德魯絲」之名儼然已經成為一個暢銷書品牌，其親族甚至聘請代筆作家，根據她留下的草稿大綱，至今仍繼續發表新作。'
+  const request = {
+    input: { text },
+    voice: { languageCode: 'yue-HK', ssmlGender: 'NEUTRAL', name:"yue-HK-Standard-B" },
+    audioConfig: { audioEncoding: 'MP3' },
+  };
 
+  try {
+    const [response] = await client.synthesizeSpeech(request);
+    const audioContent = response.audioContent;
+
+    res.set('Content-Type', 'audio/mpeg');
+    res.send(audioContent);
+  } catch (error) {
+    console.error('Error synthesizing speech:', error);
+    res.status(500).send('Error synthesizing speech');
+  }
+});
 
 
 
