3454ed0e4802a7087b73398902e7bcfa8b70b921
Edgarchung <edgar.chungmantik@gmail.com>
2024-01-16 18:51:32 +0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,39 @@ async function logout_account(){
 
   window.location.reload();
 }
+
+
+async function getUsername() {
+  // 1. fetch call /user/session
+  let getUsernameRes = await fetch("/user/session");
+
+  let getUsernameResult;
+  
+  if (getUsernameRes.status != 200){
+    getUsernameResult = await getUsernameRes.json();
+    
+  }else {
+    getUsernameResult = await getUsernameRes.json();
+  }
+
+  if(typeof getUsernameResult.data !== 'undefined'){
+    document.querySelector('.guestName').innerHTML = "Hello, "+ getUsernameResult.data;
+  }
+
+  const loginButton = document.querySelector('.loginButton')
+  const logoutButton = document.querySelector('.logout-button')
+  if(getUsernameResult.data){
+    logoutButton.classList.remove('hidden');
+    loginButton.classList.add('hidden')
+  } else {
+    logoutButton.classList.add('hidden');
+    loginButton.classList.remove('hidden')
+  }
+  // 2. if success => get response data 
+  // 3. print the username into html   
+}
+
+getUsername()
 // // const express = require('express');
 // const textToSpeech = require('@google-cloud/text-to-speech');
 // const fs = require('fs');
