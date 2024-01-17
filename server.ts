801c5bf0abcfb4c82cee9fa77026ca7193c05aa4
Edgarchung <edgar.chungmantik@gmail.com>
2024-01-17 13:09:47 +0800

    update

--- server.ts
+++ server.ts
@@ -10,27 +10,35 @@ import { is_admin } from "./middelware";
 import expressSession from "express-session";
 import { UserController } from "./controllers/user.controller";
 import { UserServiceImpl } from "./services/user.service.impl";
+export const knex = createKnex();
 import { router } from "./routes/routes";
 import { CollectController } from "./controllers/collect.controller";
 import { CollectServiceImpl } from "./services/collect.service.impl";
 import cors from "cors";
-//<--------------------------------------------------------------->
-
-export const knex = createKnex();
-
-//<--------------------------------------------------------------->
 
 const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const textToSpeech = require("@google-cloud/text-to-speech");
 const fs = require("fs");
 const keyFile = "c29-bad-grp3.json";
 const client = new textToSpeech.TextToSpeechClient({
   keyFilename: keyFile,
 });
+
+//<--------------------------March's need------------------------------------>
 let userService = new UserServiceImpl(knex);
 let collectService = new CollectServiceImpl(knex);
 
-//<--------------------------------------------------------------->
+app.use(
+  expressSession({
+    secret: "gyukj%^&*(*UYTGYHUJYT&*YHIUGYGYI",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 declare module "express-session" {
   interface SessionData {
@@ -40,22 +48,13 @@ declare module "express-session" {
     user_id: number
   }
 }
-
 //<-----------APP.USE---------------------------------------------->
 
-app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  expressSession({
-    secret: "gyukj%^&*(*UYTGYHUJYT&*YHIUGYGYI",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
 app.use(router);
 app.use(new UserController(userService).router);
 app.use(new CollectController(collectService).router);
+
+
 app.use(express.static("public/html/"));
 app.use(express.static("public"));
 app.use(express.static("books"));
@@ -64,8 +63,9 @@ app.use(express.static("public"));
 app.use(express.static("voice"));
 app.use("/admin", is_admin, express.static("private"));
 
-//<----------Convert tp mp3 , save = http://localhost:8080/----------------------------------------------------->
+//<--------------------------------------------------------------->
 
+// Convert tp mp3 , save = http://localhost:8080/
 app.get("/textToSpeech", async (req, res) => {
   const text =
   "Testing Testing";
@@ -118,8 +118,9 @@ app.listen(port, async () => {
 })
 
 
-//<----------Convert to mp3 , save = Vscode----------------------------------------------------->
+//<--------------------------------------------------------------->
 
+// Convert to mp3 , save = Vscode
 async function synthesizeSpeech() {
   const request = {
     input: {
@@ -142,3 +143,27 @@ async function synthesizeSpeech() {
 synthesizeSpeech();
 
 //<--------------------------------------------------------------->
+
+// Scraping
+async function scrapeHaodoo() {
+  try {
+    // Make a request to the website
+    const response = await axios.get("https://www.haodoo.net/");
+
+    // Load the HTML content into Cheerio
+    const $ = cheerio.load(response.data);
+
+    // Extract data based on HTML elements and classes
+    const bookTitles: string[] = [];
+    $(".a03").each((_, element) => {
+      const title = $(element).find("a").text().trim();
+      bookTitles.push(title);
+    });
+
+    // Display the extracted data
+    // console.log("Book Titles:");
+    // console.log(bookTitles);
+  } catch (error) {
+    console.error("Error!!!");
+  }
+}
